72994b40826d779eff77e7845bab0f1f327a8a9d
0andme <yu00mi97@gmail.com>
2023-01-20 10:06:53 +0900

    feat : 로그인한 사용자의 정보를 직렬화 데이터에 직접 넣어줌 - 방법 2

--- shinhanrest/product/serializers.py
+++ shinhanrest/product/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from rest_framework.exceptions import ValidationError
 from .models import Product,Comment
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -17,15 +17,39 @@ class CommentSerializer(serializers.ModelSerializer):
         fields="__all__"
 
 class CommentCreateSerializer(serializers.ModelSerializer):
-   
-    def validate(self, attrs):
+    # 1. 방법 1
+    # def validate(self, attrs):
 
-        request = self.context['request']
-        if request.user.is_authenticated:
-            attrs['member']=request.user
-        return attrs 
+    #     request = self.context.get('request')
+    #     if request and request.user.is_authenticated:
+    #         attrs['member']=request.user
+    #     else :
+    #         raise ValidationError('member is required')
+    #     # 최종 검증된 data return
+    #     return attrs 
+
+
+    # 방법 2
+    # 멤버를 가려버림
+    member=serializers.HiddenField(
+        default=serializers.CurrentUserDefault(),
+        required=False,
+        
+    )
+
+    # validate_member()/  validate_필드명 / 필드명을 검증하는데 사용하는 함수
+    # 그 필드를 알아서 검증해줌 
+    # value안에 request 유저 변수가 들어옴
+
+    def validate_member(self, value):
+        # 헤더에 토근을 넣어주지 않았을 때
+        if not value.is_authenticated :
+            raise serializers.ValidationError('member is required')
+        return value
+    
 
     class Meta:
         model=Comment
         fields="__all__"
+        # postman에서 사용자에 대한 정보를 숨김
         extra_kwargs={'member':{'required':False}}
