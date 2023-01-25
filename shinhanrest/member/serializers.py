105fc721725141d7029ddac3458ac715280d7c31
0andme <yu00mi97@gmail.com>
2023-01-25 10:48:49 +0900

    feat : serializer 이용하여 재작성

--- shinhanrest/member/serializers.py
+++ shinhanrest/member/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import Member
-
+from django.contrib.auth.hashers import make_password
 
 class MemberSerializer(serializers.ModelSerializer):
+    # 회원가입 시 패스워드가 그대로 노출되는 것을 막아보자
+    def validate_password(self, value):
+        # 유효성 검사가 끝난 값을 반환
+        if len(value) < 8 : 
+            raise serializers.ValidationError('Too short password')
+        # 암호화 하여 반환하면 된다.
+        return make_password(value)
+
     class Meta:
         model=Member
         fields="__all__"
--- shinhanrest/member/views.py
+++ shinhanrest/member/views.py
@@ -1,42 +1,42 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from django.contrib.auth.hashers import make_password
 from rest_framework.response import Response
-from rest_framework import status
-from .models import Member
-
 from rest_framework import generics,mixins,status
 from .serializers import MemberSerializer
 # Create your views here.
 
-class MemberRegisterView(APIView):
-    def post(self,request,*args,**kwargs):
-        username=request.data.get('username')
-        password=request.data.get('password')
-        tel=request.data.get('tel')
-        user_status='일반'
+class MemberRegisterView(
+    mixins.CreateModelMixin,
+    generics.GenericAPIView,
+):
+    serializer_class=MemberSerializer
 
-        if Member.objects.filter(username=username).exists():
-            return Response(
-                {'detail':'Already used'},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        member= Member(
-            username=username,
-            password=make_password(password),
-            tel=tel,
-            status=user_status)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, args, kwargs)
+
+
+# class MemberRegisterView(APIView):
+#     def post(self,request,*args,**kwargs):
+#         """
+#         POST /api/member
+#         회원가입 기능
+#         """
+#         username=request.data.get('username')
+#         password=request.data.get('password')
+#         tel=request.data.get('tel')
+#         user_status='일반'
+
+#         if Member.objects.filter(username=username).exists():
+#             return Response(
+#                 {'detail':'Already used'},
+#                 status=status.HTTP_400_BAD_REQUEST,
+#             )
+#         member= Member(
+#             username=username,
+#             password=make_password(password),
+#             tel=tel,
+#             status=user_status)
         
-        member.save()
+#         member.save()
 
-        return Response(status=status.HTTP_201_CREATED)
+#         return Response(status=status.HTTP_201_CREATED)
 
-
-# class CreateMemberView2(
-#     mixins.CreateModelMixin,
-#     generics.GenericAPIView,
-# ):
-#     serializer_class=MemberSerializer
-
-#     def create(self, request, *args, **kwargs):
-#         return self.create(request, args, kwargs)
