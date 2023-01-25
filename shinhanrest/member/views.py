7ce9c4f959625b52622071345759d8d577e6fc9b
0andme <yu00mi97@gmail.com>
2023-01-25 09:39:52 +0900

    feat : 회원가입 기능 만들기 APIView사용

--- shinhanrest/member/views.py
+++ shinhanrest/member/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from django.contrib.auth.hashers import make_password
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Member
 
 # Create your views here.
+
+class CreateMemberView(APIView):
+    def post(self,request,*args,**kwargs):
+        username=request.data.get('username')
+        password=request.data.get('password')
+        tel=request.data.get('tel')
+        user_status='일반'
+
+        member= Member(
+            username=username,
+            password=make_password(password),
+            tel=tel,
+            status=user_status)
+        
+        member.save()
+
+        return Response(status=status.HTTP_201_CREATED)
